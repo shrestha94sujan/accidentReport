fb8f6bcaf1539781f2f3337183064781207da3be
sujanshrestha <shrestha94.sujan@gmail.com>
2020-03-27 08:44:10 -0500

    created chart-visuals component for hosting all the chart visualizations

---
+++ src/app/chart-visuals/chart-visuals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-visuals',
+  templateUrl: './chart-visuals.component.html',
+  styleUrls: ['./chart-visuals.component.css']
+})
+export class ChartVisualsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
