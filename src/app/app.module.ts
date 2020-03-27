c72accd24a2401733d95264890b133d52b23b364
sujanshrestha <shrestha94.sujan@gmail.com>
2020-03-27 15:39:55 -0500

    added httpClientModule for data read and write

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ChartVisualsComponent } from './chart-visuals/chart-visuals.component';
@@ -10,7 +11,8 @@ import { ChartVisualsComponent } from './chart-visuals/chart-visuals.component';
     ChartVisualsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
