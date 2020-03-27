fc3f2b5d94969bdb96bc1c0a5f32a6f18063cef5
sujanshrestha <shrestha94.sujan@gmail.com>
2020-03-27 08:44:33 -0500

    added the chart-visuals component to the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartVisualsComponent } from './chart-visuals/chart-visuals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartVisualsComponent
   ],
   imports: [
     BrowserModule
